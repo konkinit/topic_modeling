5f0627c581d067e747bde16a38f3430db8305a10
ikonkobo <ikonkobo.idrissa@gmail.com>
2023-05-17 13:44:36 +0000

    setting tests files

--- tests/test_data_preprocess.py
+++ tests/test_data_preprocess.py
@@ -1,15 +1,9 @@
 import os
 import sys
-import pytest
 
 if os.getcwd() not in sys.path:
     sys.path.append(os.getcwd())
-from src.data_preprocess import Preprocessing
 
 
-@pytest.mark.parametrize(
-    ('language', 'text'),
-    [()]
-)
 def test_Preprocessing():
     pass
